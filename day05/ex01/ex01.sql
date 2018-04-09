ae4e3ba65c971602b91f6920b2d7a23f4a1bfe16
Julia <julyettka@gmail.com>
2018-04-09 19:23:35 +0300

    day 05: ex00-ex02

---
+++ day05/ex01/ex01.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ft_table (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+login VARCHAR(8) DEFAULT 'toto' NOT NULL,
+`group` ENUM('student', 'staff', 'other') NOT NULL,
+email VARCHAR(50) NOT NULL,
+creation_date DATE NOT NULL
+)
